27e944ab30bf15675911fd9365e33d87b4909839
Ben Helleman <bhellema@adobe.com>
2025-02-24 14:18:02 -0500

    chore: trying modal

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -47,6 +47,18 @@ export function moveInstrumentation(from, to) {
   );
 }
 
+function autolinkModals(element) {
+  element.addEventListener('click', async (e) => {
+    const origin = e.target.closest('a');
+
+    if (origin && origin.href && origin.href.includes('/modals/')) {
+      e.preventDefault();
+      const { openModal } = await import(`${window.hlx.codeBasePath}/blocks/modal/modal.js`);
+      openModal(origin.href);
+    }
+  });
+}
+
 /**
  * load fonts.css and set a session storage flag
  */
@@ -117,6 +129,8 @@ async function loadEager(doc) {
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
+  autolinkModals(doc);
+
   const main = doc.querySelector('main');
   await loadSections(main);
 
