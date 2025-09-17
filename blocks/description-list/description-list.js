fef4ee451b26df52eb22de0ecf1cb8cf006468f6
Ben Helleman <bhellema@adobe.com>
2025-09-17 11:54:37 -0400

    description list

---
+++ blocks/description-list/description-list.js
@@ -0,0 +1,15 @@
+export default async function decorate(block) {
+  // given a dom structure of key value pairs, create a description list
+  const descriptionList = document.createElement('dl');
+  descriptionList.classList.add('description-list');
+  [...block.children].forEach((row) => {
+    const dt = document.createElement('dt');
+    dt.textContent = row.querySelector(':scope > div:first-child').textContent;
+    const dd = document.createElement('dd');
+    dd.textContent = row.querySelector(':scope > div:last-child').textContent;
+    descriptionList.appendChild(dt);
+    descriptionList.appendChild(dd);
+  });
+  block.textContent = '';
+  block.append(descriptionList);
+}
