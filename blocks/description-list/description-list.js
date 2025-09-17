b55fc17bc8ec577b9e7ba29c1be503f21f6a7aa6
Ben Helleman <bhellema@adobe.com>
2025-09-17 12:28:27 -0400

    description list

--- blocks/description-list/description-list.js
+++ blocks/description-list/description-list.js
@@ -1,20 +1,20 @@
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
-  // given a dom structure of key value pairs, create a description list
-  const descriptionList = document.createElement('dl');
-  descriptionList.classList.add('description-list');
-  [...block.children].forEach((row) => {
-    const dt = document.createElement('dt');
-    dt.textContent = row.querySelector(':scope > div:first-child').textContent;
-    const dd = document.createElement('dd');
-    dd.textContent = row.querySelector(':scope > div:last-child').textContent;
-    descriptionList.appendChild(dt);
-    descriptionList.appendChild(dd);
-  });
-  block.textContent = '';
+  // // given a dom structure of key value pairs, create a description list
+  // const descriptionList = document.createElement('dl');
+  // descriptionList.classList.add('description-list');
+  // [...block.children].forEach((row) => {
+  //   const dt = document.createElement('dt');
+  //   dt.textContent = row.querySelector(':scope > div:first-child').textContent;
+  //   const dd = document.createElement('dd');
+  //   dd.textContent = row.querySelector(':scope > div:last-child').textContent;
+  //   descriptionList.appendChild(dt);
+  //   descriptionList.appendChild(dd);
+  // });
+  // block.textContent = '';
 
-  moveInstrumentation(block, descriptionList);
+  // moveInstrumentation(block, descriptionList);
 
-  block.append(descriptionList);
+  // block.append(descriptionList);
 }
