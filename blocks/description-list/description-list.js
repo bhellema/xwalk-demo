d65ff593b28ab886443a9c944afc76c807533027
Ben Helleman <bhellema@adobe.com>
2025-09-17 12:35:56 -0400

    description list
    description list
    
    description list
    
    description list
    
    description list
    
    description list
    
    description list
    
    description list

--- blocks/description-list/description-list.js
+++ blocks/description-list/description-list.js
@@ -1,20 +1,26 @@
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
-  // // given a dom structure of key value pairs, create a description list
-  // const descriptionList = document.createElement('dl');
-  // descriptionList.classList.add('description-list');
-  // [...block.children].forEach((row) => {
-  //   const dt = document.createElement('dt');
-  //   dt.textContent = row.querySelector(':scope > div:first-child').textContent;
-  //   const dd = document.createElement('dd');
-  //   dd.textContent = row.querySelector(':scope > div:last-child').textContent;
-  //   descriptionList.appendChild(dt);
-  //   descriptionList.appendChild(dd);
-  // });
-  // block.textContent = '';
+  // given a dom structure of key value pairs, create a description list
+  const descriptionList = document.createElement('dl');
+  descriptionList.classList.add('description-list');
+  [...block.children].forEach((row) => {
+    const rowItem = document.createElement('div');
+    rowItem.classList.add('description-list-item');
+    moveInstrumentation(row, rowItem);
 
-  // moveInstrumentation(block, descriptionList);
+    const dt = document.createElement('dt');
+    moveInstrumentation(row.querySelector(':scope > div:first-child > div'), dt);
+    dt.textContent = row.querySelector(':scope > div:first-child').textContent;
+    const dd = document.createElement('dd');
+    moveInstrumentation(row.querySelector(':scope > div:last-child > div'), dd);
+    dd.textContent = row.querySelector(':scope > div:last-child').textContent;
 
-  // block.append(descriptionList);
+    rowItem.appendChild(dt);
+    rowItem.appendChild(dd);
+    descriptionList.appendChild(rowItem);
+  });
+  block.textContent = '';
+
+  block.append(descriptionList);
 }
