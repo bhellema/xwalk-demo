80f7295a9f703eabf45d28742ca117b68fc77e26
Ben Helleman <bhellema@adobe.com>
2025-09-18 07:45:32 -0400

    simp

--- blocks/description-list/description-list.js
+++ blocks/description-list/description-list.js
@@ -5,10 +5,6 @@ export default async function decorate(block) {
   const descriptionList = document.createElement('dl');
   descriptionList.classList.add('description-list');
   [...block.children].forEach((row) => {
-    const rowItem = document.createElement('div');
-    rowItem.classList.add('description-list-item');
-    moveInstrumentation(row, rowItem);
-
     const dt = document.createElement('dt');
     moveInstrumentation(row.querySelector(':scope > div:first-child > div'), dt);
     dt.textContent = row.querySelector(':scope > div:first-child').textContent;
@@ -16,9 +12,8 @@ export default async function decorate(block) {
     moveInstrumentation(row.querySelector(':scope > div:last-child > div'), dd);
     dd.textContent = row.querySelector(':scope > div:last-child').textContent;
 
-    rowItem.appendChild(dt);
-    rowItem.appendChild(dd);
-    descriptionList.appendChild(rowItem);
+    descriptionList.appendChild(dt);
+    descriptionList.appendChild(dd);
   });
   block.textContent = '';
 
