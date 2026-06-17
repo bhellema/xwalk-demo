71cc24034906777741a00ad1d8f5a050d87c64b7
Ben Helleman <bhellema@adobe.com>
2026-06-16 21:16:39 -0400

    add cols

--- tools/importer/transformers/index.js
+++ tools/importer/transformers/index.js
@@ -4,6 +4,7 @@ import cards from './cards.js';
 import teaser from './teaser.js';
 import featured from './featured.js';
 import carousel from './carousel.js';
+import myColumnsBen from './columns-ben.js';
 
 // eslint-disable-next-line import/prefer-default-export
 export const transformers = [
@@ -12,6 +13,7 @@ export const transformers = [
   cards,
   teaser,
   carousel,
+  myColumnsBen,
 ];
 
 export const postTransformers = [
