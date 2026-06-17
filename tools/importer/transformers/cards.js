0f0d8b27807d469832eb485d01d13f5015bb5155
Ben Helleman <bhellema@adobe.com>
2026-06-16 21:27:20 -0400

    update card parser

--- tools/importer/transformers/cards.js
+++ tools/importer/transformers/cards.js
@@ -1,33 +1,22 @@
 /* global WebImporter */
-import { cleanUpSrcUrl } from '../utils/image-utils.js';
-
 const createCards = (main, document, params) => {
   const { originalURL } = params;
-  const cards = main.querySelector('.cards');
+  const cardContainer = main.querySelector('.cards');
+  const cards = main.querySelectorAll('.cards > div');
 
   const cellItems = [];
   if (cards) {
-    const ul = cards.querySelector('ul');
-    for (let i = 0; i < ul.children.length; i += 1) {
-      const cardItem = ul.children[i];
-      const picImg = cardItem.querySelector('picture > img');
-      picImg.src = cleanUpSrcUrl(picImg.src, originalURL);
-      picImg.alt = picImg.alt || 'Plush Item';
-
-      const title = cardItem.querySelector('.cards-card-body > p > strong');
-      const body = cardItem.querySelector('.cards-card-body > p:nth-of-type(2)');
-
-      const p = document.createElement('p');
-      p.appendChild(title);
-      p.appendChild(body);
-      cellItems.push([picImg, p]);
-    }
+    cards.forEach((cardItem) => {
+      const picture = cardItem.querySelector(':scope > div:nth-of-type(1) > picture');
+      const body = cardItem.querySelector(':scope > div:nth-of-type(2)');
+      cellItems.push([picture, body]);
+    });
 
     const cells = [['Cards'], ...cellItems];
 
     const block = WebImporter.DOMUtils.createTable(cells, document);
-    cards.innerHTML = '';
-    cards.append(block);
+    cardContainer.innerHTML = '';
+    cardContainer.append(block);
   }
 };
 export default createCards;
